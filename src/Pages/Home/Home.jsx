89d28cb2429b79c04b51d46ba481334655901479
rafa <rafius>
2021-11-20 20:38:46 +0100

    Add component to render the events

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,24 @@
-import * as React from "react";
-import Matches from "@/Components/Matches";
+import React from "react";
+import Header from "@/Components/Header/Header";
+import Events from "@/Components/Events";
+import { Main } from "./HomeStyled";
+import { schedule } from "./schedule.json";
+import useHomeHooks from "./HomeHooks";
 
 const Home = () => {
-  return <Matches />;
+  const { week, increaseWeek, decreaseWeek } = useHomeHooks();
+
+  return (
+    <Main>
+      <Header
+        schedule={schedule}
+        week={week}
+        increaseWeek={increaseWeek}
+        decreaseWeek={decreaseWeek}
+      />
+      <Events schedule={schedule} week={week} />
+    </Main>
+  );
 };
 
 export default Home;
