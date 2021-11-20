97f6de22ae2f4fd7f7b0c6512fa2c3d6fb292d33
rafa <rafius>
2021-11-20 22:24:26 +0100

    Redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import Home from "./pages/Home/Home";
+import Home from "@/pages/Home";
 
 import configureStore from "./redux/store";
 
