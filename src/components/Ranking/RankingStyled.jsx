5cfa1ff666fa1ad2e2656cbb514121cdaa6616c5
rafa <rafius>
2021-12-13 16:09:17 +0100

    Update Ranking stlye

--- src/components/Ranking/RankingStyled.jsx
+++ src/components/Ranking/RankingStyled.jsx
@@ -1,16 +1,15 @@
 import { styled } from "@mui/material/styles";
 import TableRow from "@mui/material/TableRow";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import { red } from "@mui/material/colors";
 
 export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  whiteSpace: "nowrap",
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 16,
-    whiteSpace: "nowrap",
   },
 }));
 
